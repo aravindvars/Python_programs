26184d0bec040283651c3f82a25303601eed70c4
Aravind <aravindramsharma@gmail.com>
2023-05-25 15:06:06 +0530

    optimised the program a little bit

--- grade.py
+++ grade.py
@@ -1,16 +1,16 @@
 marks = int(input('Student\'s mark is:'))
 
-if 90 <= marks <= 100:
+if marks >= 90:
     print('S grade')
-elif 80 <= marks < 90:
+elif marks >= 80:
     print('A grade')
-elif 70 <= marks < 80:
+elif marks >= 70:
     print('B grade')
-elif 60 <= marks < 70:
+elif marks >= 60:
     print('C grade')
-elif 50 <= marks < 60:
+elif marks >= 50:
     print('D grade')
-elif 40 <= marks < 50:
+elif marks >= 40:
     print('E grade')
 else:
     print('F grade')

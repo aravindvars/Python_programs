c1d5a0f2206d673b33e16fbd131ea3b126505fea
Aravind <aravindramsharma@gmail.com>
2023-08-06 15:36:52 +0530

    Restricting user changes by getter & setter

--- OOPS/student.py
+++ OOPS/student.py
@@ -1,39 +1,46 @@
 class Student: 
-    def __init__(self, name, house, patronus):
-      if not name:
-        raise ValueError('Missing Name')
-      if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
-        raise ValueError("House name Invalid")
+    def __init__(self, name, house):
       self.name = name
       self.house = house
-      self.patronus = patronus
+      
     
     def __str__(self):
       return f'{self.name} from {self.house}' 
     
-    def charm(self):
-      match self.patronus:
-        case 'Stag':
-          return ':S'
-        case 'Otter':
-          return ':O'
-        case _:
-          return 'Puppy'
-          
-        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError('Missing Name')
+
+
+    #Getter function
+    @property
+    def house(self):
+      return self._house
+    
+    #Setter fucntion --> This is used to prevent the programmer to change attributes which we don't wanr to allow for change
+    @house.setter
+    def house(self, house):
+      if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
+        raise ValueError ('Invalid House name')
+      self._house = house # The instance variable is used with an underscore.
   
 
 def main():
-  student = get_student()
-  print('Expecto Patronum')
-  print(student.charm())
+    student = get_student()
+    #student.house = 'Green'  Now this will not happen as we set the setter function to prevent
+    print(student) 
+  
   
 
 def get_student():
   name = input('Name: ')
   house = input('House: ')
-  patronus = input('Patronus: ')
-  return Student(name,house, patronus) 
+  return Student(name,house) 
   
 
 if __name__ == '__main__':

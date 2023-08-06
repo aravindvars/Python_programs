8c704bc301f9c1bdbc0e2b54695d8dd24a661c55
Aravind <aravindramsharma@gmail.com>
2023-08-06 09:40:51 +0530

    Adding method to our Student class and calling it

--- OOPS/student.py
+++ OOPS/student.py
@@ -1,29 +1,40 @@
 class Student: 
-    def __init__(self, name, house): # __init__ is the inititalization of the object in python. "New" method of python creates the object, which runs in the background and this init method initialises it. We as a programmer need to worry about the init method only. Python takes care of new
+    def __init__(self, name, house, patronus):
       if not name:
         raise ValueError('Missing Name')
       if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
         raise ValueError("House name Invalid")
       self.name = name
       self.house = house
-    
+      self.patronus = patronus
     
     def __str__(self):
-      return f'{self.name} from {self.house}' # This __str__ gets called when the object location is called.
+      return f'{self.name} from {self.house}' 
+    
+    def charm(self):
+      match self.patronus:
+        case 'Stag':
+          return ':S'
+        case 'Otter':
+          return ':O'
+        case _:
+          return 'Puppy'
+          
+        
   
-
 
 def main():
   student = get_student()
-  print(student) # This references the object in the memory and print out it's location
+  print('Expecto Patronum')
+  print(student.charm())
   
 
 def get_student():
   name = input('Name: ')
   house = input('House: ')
-  return Student(name,house) # This is a constructor call. This is going to construct a student object for us. student object is created by using the Student class as template
+  patronus = input('Patronus: ')
+  return Student(name,house, patronus) 
   
-
 
 if __name__ == '__main__':
   main()

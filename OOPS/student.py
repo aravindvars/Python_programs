038848671c6ceb3dbd2dc59c771207d8486c29f8
Aravind <aravindramsharma@gmail.com>
2023-08-06 09:26:22 +0530

    Adding the class, the init method, the str method.

--- OOPS/student.py
+++ OOPS/student.py
@@ -1,17 +1,28 @@
-class Student: # class is a blueprint, a mould. A class can be termed as a data type that we have created in our code to match our requirements
-  ...
+class Student: 
+    def __init__(self, name, house): # __init__ is the inititalization of the object in python. "New" method of python creates the object, which runs in the background and this init method initialises it. We as a programmer need to worry about the init method only. Python takes care of new
+      if not name:
+        raise ValueError('Missing Name')
+      if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
+        raise ValueError("House name Invalid")
+      self.name = name
+      self.house = house
+    
+    
+    def __str__(self):
+      return f'{self.name} from {self.house}' # This __str__ gets called when the object location is called.
+  
 
 
 def main():
   student = get_student()
-  print(f'{student.name} from {student.house}')
+  print(student) # This references the object in the memory and print out it's location
   
 
 def get_student():
-  student = Student() # object is when you use the mould to build something
-  student.name = input('Name: ')
-  student.house = input('House: ')
-  return student
+  name = input('Name: ')
+  house = input('House: ')
+  return Student(name,house) # This is a constructor call. This is going to construct a student object for us. student object is created by using the Student class as template
+  
 
 
 if __name__ == '__main__':

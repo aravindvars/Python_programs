84598df853df35cf833a92f3cf1172beac34cc4f
Aravind <aravindramsharma@gmail.com>
2023-08-07 14:49:04 +0530

    Modified student.py & hat.py usign @classmethod

--- OOPS/hat.py
+++ OOPS/hat.py
@@ -1,16 +1,16 @@
 import random
 
 class Hat:
-  def __init__(self):
-      self.houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']
-    
   
-  def sort(self, name):
-      house = random.choice(self.houses)
-      print(name, 'is in', house)
+      houses = ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin'] # houses is now a class varibale and not a instant variable
+    
+      @classmethod
+      def sort(cls, name):
+            house = random.choice(cls.houses)
+            print(name, 'is in', house)
 
-hat = Hat()
-hat.sort('Harry')
+# hat = Hat() Now we need not instantiate the hat object to a class Hat. We can directly refer the class now
+Hat.sort('Harry')
 
       
       
--- OOPS/student.py
+++ OOPS/student.py
@@ -7,42 +7,39 @@ class Student:
     def __str__(self):
       return f'{self.name} from {self.house}' 
     
-    @property
-    def name(self):
-        return self._name
+    # @property
+    # def name(self):
+    #     return self.name
     
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError('Missing Name')
+    # @name.setter
+    # def name(self, name):
+    #     if not name:
+    #         raise ValueError('Missing Name')
 
 
-    #Getter function
-    @property
-    def house(self):
-      return self._house
+    # #Getter function
+    # @property
+    # def house(self):
+    #   return self.house
     
-    #Setter fucntion --> This is used to prevent the programmer to change attributes which we don't wanr to allow for change
-    @house.setter
-    def house(self, house):
-      if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
-        raise ValueError ('Invalid House name')
-      self._house = house # The instance variable is used with an underscore.
+    # #Setter fucntion --> This is used to prevent the programmer to change attributes which we don't wanr to allow for change
+    # @house.setter
+    # def house(self, house):
+    #   if house not in ['Gryffindor', 'Hufflepuff', 'Ravenclaw', 'Slytherin']:
+    #     raise ValueError ('Invalid House name')
+    #   self._house = house # The instance variable is used with an underscore.
+    
+    @classmethod  
+    def get(cls):
+        name = input('Name: ')
+        house = input('House: ')
+        return cls(name,house) 
   
 
 def main():
-    student = get_student()
-    #student.house = 'Green'  Now this will not happen as we set the setter function to prevent
+    student = Student.get()
     print(student) 
   
-  
-
-def get_student():
-  name = input('Name: ')
-  house = input('House: ')
-  return Student(name,house) 
-  
-
 if __name__ == '__main__':
   main()
   
